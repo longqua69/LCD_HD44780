da51fba4e780ff25f35e9355d0430be8bc752013
longqua69 <37626565+longqua69@users.noreply.github.com>
2020-08-03 22:19:56 +0700

    Create macros for LCD instructions set

--- HD_44780/src/main.c
+++ HD_44780/src/main.c
@@ -1,5 +1,13 @@
 #include <avr/io.h>
 
+/* Refer to page 24/59 HD44780 datasheet */
+#define LCD_CLear_Display               0x01
+#define LCD_Return_Home                 0x02
+#define LCD_Entry_Mode_Set              0x07
+#define LCD_Display_On_Off_Control      0x0F
+#define LCD_Cursor_Display_Shift        0x1C
+#define LCD_Function_Set                0x2C    // Configure 4-bit mode, 2 lines, 5x10 dots matrix
+
 int main(void)
 {
 
